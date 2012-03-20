729acadd78a34cd96e8f32c31c0c7a03b3e820d1
brad <brad@e5403b57-782b-4058-b618-70b0ecfe8436>
2012-03-20 18:50:13 +0000

    Updated POMs for building in Bamboo.
    git-svn-id: https://scm.infusiontest.com/svn/cas/trunk@68638 e5403b57-782b-4058-b618-70b0ecfe8436

---
+++ src/test/java/com/infusiontest/cas/SampleTest.java
@@ -0,0 +1,12 @@
+package com.infusiontest.cas;
+
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testFake() {
+        Assert.assertTrue(true);
+    }
+}
