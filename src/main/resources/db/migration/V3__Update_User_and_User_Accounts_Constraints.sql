ad2fdb3fe6af048f4a5ac078ab20e25b7329c840
kevin.wise <kevin.wise@e5403b57-782b-4058-b618-70b0ecfe8436>
2013-04-17 23:17:35 +0000

    Fix SQL script
    git-svn-id: https://scm.infusiontest.com/svn/cas/trunk@88465 e5403b57-782b-4058-b618-70b0ecfe8436

--- src/main/resources/db/migration/V3__Update_User_and_User_Accounts_Constraints.sql
+++ src/main/resources/db/migration/V3__Update_User_and_User_Accounts_Constraints.sql
@@ -8,7 +8,7 @@ ALTER TABLE `user_account` ADD UNIQUE KEY `app_type_name_username` (`app_type`, 
 */
 
 ALTER TABLE `user_account`
-	CHANGE COLUMN `disabled` `enabled` BIT(1) NOT NULL,
+	CHANGE COLUMN `disabled` `disabled` BIT(1) NOT NULL,
 	CHANGE COLUMN `user_id` `user_id` BIGINT(20) NOT NULL;
 
 ALTER TABLE `user`
