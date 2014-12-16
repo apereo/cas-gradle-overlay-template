08a063e546ccfe0e30062094e1603b2ad680377e
Kevin Wise <kevin.wise@infusionsoft.com>
2014-12-16 10:38:24 -0700

    CAS-96 - CAS integration for Partner Portal

--- src/main/resources/db/migration/V22__Update_Partner_Portal_Service.sql
+++ src/main/resources/db/migration/V22__Update_Partner_Portal_Service.sql
@@ -1,5 +1,3 @@
 UPDATE RegisteredServiceImpl set
  serviceId = 'https://(((dev|test|live)-is-partners\\.pantheon\\.io)|(portal\\.infusionsoft\\.com))/.*'
 where name = 'PartnerPortal';
-
-https://(((dev|test|live)-is-partners\.pantheon\.io)|(portal\.infusionsoft\.com))/.*
