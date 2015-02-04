7f9f28cd8629df671f857a7cfd197b280eac60bc
Bradley Booth <bradley.booth@gmail.com>
2015-02-04 13:44:13 -0700

    adding sample spec

---
+++ src/test/groovy/com/infusionsoft/cas/auth/OAuthTicketGrantingTicketAuthenticationProviderSpec.groovy
@@ -0,0 +1,14 @@
+import spock.lang.Specification
+
+public class OAuthTicketGrantingTicketAuthenticationProviderSpec extends Specification {
+    public "test something"() {
+        when:
+        int x = 1
+        int y = 2
+
+        then:
+        x != y
+
+
+    }
+}
