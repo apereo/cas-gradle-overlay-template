96c443ebd51c885a543468541be9737da4f5e29c
Bradley Booth <bradley.booth@gmail.com>
2015-05-04 20:00:03 -0700

    Previous 4 passwords not verfied on check off list

--- src/main/webapp/js/infusionsoft-chosen.js
+++ src/main/webapp/js/infusionsoft-chosen.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
         '.chosen-select-no-single' : {disable_search_threshold:10},
         '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
         '.chosen-select-width'     : {width:"95%"}
-    }
+    };
+
     for (var selector in config) {
         $(selector).chosen(config[selector]);
     }
+
     $(".chosen-container").css({ width: '100%' }); // Overrides chosen.js's hard-coded width, so that the dropdown is truly responsive
 });
