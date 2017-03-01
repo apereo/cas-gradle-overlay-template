d36002668df623bd58f74b7e2e792462c5eff627
Bradley Booth <bradley.booth@gmail.com>
2017-02-28 23:37:14 -0700

    AUTH-17: Implement Discourse SSO

---
+++ src/main/java/com/infusionsoft/cas/web/controllers/commands/DiscourseSSOCommand.java
@@ -0,0 +1,22 @@
+package com.infusionsoft.cas.web.controllers.commands;
+
+public class DiscourseSSOCommand {
+    private String sso;
+    private String sig;
+
+    public String getSso() {
+        return sso;
+    }
+
+    public void setSso(String sso) {
+        this.sso = sso;
+    }
+
+    public String getSig() {
+        return sig;
+    }
+
+    public void setSig(String sig) {
+        this.sig = sig;
+    }
+}
