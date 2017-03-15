96bed0bee3965ce814689de9b5209ce850c11a67
glenda.gonzales <glenda.gonzales@infusionsoft.com>
2017-03-15 10:00:32 -0700

    FS-5076 Update terms and conditions work flow

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
             errorElement: "span",
             errorClass: "help-block",
             errorPlacement: function (error, element) {
-                console.log('error placement');
                 element.after('<span class="is-icon is-icon-error form-control-feedback"><object type="image/svg+xml" tabindex="-1" data="/img/ic-message-danger.svg?b=${version}" width="4" height="16"></object></span>');
                 element.after(error);
 
