6f3caf9015b64b385bc4ad76afa17e1c6b198f20
Kevin Wise <kevin.wise@infusionsoft.com>
2017-03-17 17:43:50 -0700

    AUTH-26 - Added OAuthClientCredentialsTokenProviderTest.

--- src/test/java/com/infusionsoft/cas/auth/OAuthClientCredentialsTokenProviderTest.java
+++ src/test/java/com/infusionsoft/cas/auth/OAuthClientCredentialsTokenProviderTest.java
@@ -1,9 +1,9 @@
 package com.infusionsoft.cas.auth;
 
+import com.infusionsoft.cas.oauth.exceptions.OAuthInvalidRequestException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -11,33 +11,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class OAuthClientCredentialsTokenProviderTest {
+    private static final String scope = "full";
+    private static final String application = "application";
+    private static final String grantType = "client_credentials";
+    private static final String clientId = "clientId";
+    private static final String clientSecret = "clientSecret";
+
     @Mock
     private HttpServletRequest req;
     @Mock
     private HttpServletResponse resp;
-    @InjectMocks
-    private OAuthClientCredentialsTokenProvider oAuthClientCredentialsTokenProvider;
+
+    private OAuthClientCredentialsTokenProvider tokenProviderToTest = new OAuthClientCredentialsTokenProvider();
 
     @Before
     public void beforeMethod() {
-        oAuthClientCredentialsTokenProvider = new OAuthClientCredentialsTokenProvider();
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testValidToken() {
-        OAuthAuthenticationToken authToken = oAuthClientCredentialsTokenProvider.createAuthenticationToken(req, resp, "full", "application", "client_credentials", null, "clientId", "clientSecret");
-        Assert.assertEquals("clientId", authToken.getClientId());
-        Assert.assertEquals("full", authToken.getScope());
-        Assert.assertEquals("application", authToken.getApplication());
-        Assert.assertEquals("client_credentials", authToken.getGrantType());
+        OAuthAuthenticationToken authToken = tokenProviderToTest.createAuthenticationToken(req, resp, scope, application, grantType, null, clientId, clientSecret);
+        Assert.assertEquals(clientId, authToken.getClientId());
+        Assert.assertEquals(scope, authToken.getScope());
+        Assert.assertEquals(application, authToken.getApplication());
+        Assert.assertEquals(grantType, authToken.getGrantType());
         Assert.assertNull(authToken.getPrincipal());
-        Assert.assertEquals("clientSecret", authToken.getClientSecret());
+        Assert.assertEquals(clientSecret, authToken.getClientSecret());
     }
 
     @Test
-    public void testInValidGrantType() {
-        OAuthAuthenticationToken authToken = oAuthClientCredentialsTokenProvider.createAuthenticationToken(req, resp, "full", "application", "invalid_grant_type", null, "clientId", "clientSecret");
+    public void testInvalidGrantType() {
+        OAuthAuthenticationToken authToken = tokenProviderToTest.createAuthenticationToken(req, resp, scope, application, "invalid_grant_type", null, clientId, clientSecret);
         Assert.assertTrue(authToken == null);
     }
+
+    @Test(expected = OAuthInvalidRequestException.class)
+    public void testMissingClientId() {
+        tokenProviderToTest.createAuthenticationToken(req, resp, scope, application, grantType, null, "", clientSecret);
+    }
+
+    @Test(expected = OAuthInvalidRequestException.class)
+    public void testMissingClientSecret() {
+        tokenProviderToTest.createAuthenticationToken(req, resp, scope, application, grantType, null, clientId, "");
+    }
+
 }
