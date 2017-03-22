2e9382dc3700cfb1dee54d5f9fb02423437ae9ff
Kevin Wise <kevin.wise@infusionsoft.com>
2017-03-22 11:23:12 -0700

    AUTH-29 - remove redirect view handling (defer until we have a better understanding of what's going on)

--- src/main/java/com/infusionsoft/cas/auth/OAuthExceptionHandler.java
+++ src/main/java/com/infusionsoft/cas/auth/OAuthExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.infusionsoft.cas.auth;
 
+import com.infusionsoft.cas.oauth.dto.OAuthGrantType;
 import com.infusionsoft.cas.oauth.exceptions.OAuthException;
 import com.infusionsoft.cas.oauth.exceptions.OAuthServerErrorException;
 import org.apache.commons.lang3.StringUtils;
@@ -9,13 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
-import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,21 +25,22 @@ public class OAuthExceptionHandler extends AbstractHandlerExceptionResolver {
     private final static String ERROR_PARAMETER = "error";
     private final static String ERROR__DESCRIPTION_PARAMETER = "error_description";
     private final static String ERROR__URI_PARAMETER = "error_uri";
+    private final static String RESPONSE_TYPE_PARAMETER = "response_type";
     private final static String REDIRECT_URI_PARAMETER = "redirect_uri";
     private final static String STATE_PARAMETER = "state";
 
     private Logger logger = LoggerFactory.getLogger(OAuthExceptionHandler.class);
 
     @Autowired
-    private MessageSource messageSource;
+    MessageSource messageSource;
 
     @Override
     protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object o, Exception e) {
         ModelAndView modelAndView = new ModelAndView();
-        Map<String, String> model = new HashMap<>();
+        Map<String, String> model = new HashMap<String, String>();
 
         OAuthException oAuthException;
-        if (e instanceof OAuthException) {
+        if(e instanceof OAuthException) {
             oAuthException = (OAuthException) e;
         } else {
             oAuthException = new OAuthServerErrorException(e);
@@ -67,36 +65,13 @@ public class OAuthExceptionHandler extends AbstractHandlerExceptionResolver {
             model.put(ERROR__URI_PARAMETER, oAuthException.getErrorUri());
         }
 
-        // Errors from authorization code generation are returned via query parameters appended to the redirect URI
-        // See https://tools.ietf.org/html/rfc6749#section-4.1.2.1
-        if (StringUtils.equals(request.getPathInfo(), "/oauth/processAuthorization")) {
+        OAuthGrantType grantType = OAuthGrantType.fromValue(request.getParameter(RESPONSE_TYPE_PARAMETER));
+
+        if (OAuthGrantType.AUTHORIZATION_CODE.equals(grantType)) {
             model.put(STATE_PARAMETER, request.getParameter(STATE_PARAMETER));
-
-            final String redirectUri = request.getParameter(REDIRECT_URI_PARAMETER);
-            if (StringUtils.isBlank(redirectUri)) {
-                // If there's no redirect URI we have nowhere to redirect to, so just show a text error message
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.setContentType("text/plain");
-                modelAndView.setView(new View() {
-                    @Override
-                    public String getContentType() {
-                        return "text/plain";
-                    }
-
-                    @Override
-                    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-                        response.getWriter().write("Missing redirect_uri");
-                    }
-                });
-            } else {
-                RedirectView redirectView = new RedirectView(redirectUri);
-                redirectView.setStatusCode(HttpStatus.FOUND);
-                redirectView.setExposeModelAttributes(true);
-                modelAndView.setView(redirectView);
-            }
+            modelAndView.setViewName("redirect:" + request.getParameter(REDIRECT_URI_PARAMETER));
         } else {
             response.setStatus(oAuthException.getHttpStatus().value());
-            modelAndView.setView(new MappingJackson2JsonView());
         }
 
         modelAndView.addAllObjects(model);
