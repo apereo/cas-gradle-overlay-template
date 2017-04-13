c1901d7c77acd06d4cc9d6def1b83e14c8bd1c6b
Bradley Booth <bradley.booth@gmail.com>
2017-04-13 11:49:51 -0700

    fix attribute relase

---
+++ cas/src/main/java/org/apereo/cas/api/UserAccountDTO.java
@@ -0,0 +1,82 @@
+package org.apereo.cas.api;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apereo.cas.infusionsoft.domain.UserAccount;
+import org.apereo.cas.infusionsoft.support.AppHelper;
+
+import java.util.Collection;
+
+/**
+ * Represents an account record.
+ */
+@JsonTypeName("UserAccount")
+public class UserAccountDTO {
+    private String appType; //TODO: change back to AppType enum when case sensitivity in authenticateUser doesn't matter
+    private String appName;
+    private String appUsername;
+    private String appAlias;
+    private String appUrl;
+
+    public static UserAccountDTO[] convertFromCollection(final Collection<UserAccount> accountCollection, AppHelper appHelper) {
+        UserAccountDTO[] accounts = new UserAccountDTO[accountCollection.size()];
+        int i = 0;
+        for (UserAccount userAccount : accountCollection) {
+            accounts[i++] = new UserAccountDTO(userAccount, appHelper);
+        }
+        return accounts;
+    }
+
+    public UserAccountDTO() {
+        // For de-serialization
+    }
+
+    public UserAccountDTO(UserAccount userAccount, AppHelper appHelper) {
+        this.appType = ObjectUtils.toString(userAccount.getAppType());
+        this.appName = userAccount.getAppName();
+        this.appUsername = userAccount.getAppUsername();
+        this.appAlias = StringUtils.defaultIfEmpty(userAccount.getAlias(), userAccount.getAppName());
+        this.appUrl = appHelper.buildAppUrl(userAccount.getAppType(), userAccount.getAppName());
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+
+    public void setAppType(String appType) {
+        this.appType = appType;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppUsername() {
+        return appUsername;
+    }
+
+    public void setAppUsername(String appUsername) {
+        this.appUsername = appUsername;
+    }
+
+    public String getAppAlias() {
+        return appAlias;
+    }
+
+    public void setAppAlias(String appAlias) {
+        this.appAlias = appAlias;
+    }
+
+    public String getAppUrl() {
+        return appUrl;
+    }
+
+    public void setAppUrl(String appUrl) {
+        this.appUrl = appUrl;
+    }
+}
--- cas/src/main/java/org/apereo/cas/infusionsoft/authentication/InfusionsoftAuthenticationHandler.java
+++ cas/src/main/java/org/apereo/cas/infusionsoft/authentication/InfusionsoftAuthenticationHandler.java
@@ -1,16 +1,27 @@
 package org.apereo.cas.infusionsoft.authentication;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apereo.cas.api.UserAccountDTO;
 import org.apereo.cas.authentication.HandlerResult;
 import org.apereo.cas.authentication.UsernamePasswordCredential;
 import org.apereo.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
+import org.apereo.cas.infusionsoft.domain.User;
+import org.apereo.cas.infusionsoft.domain.UserAccount;
 import org.apereo.cas.infusionsoft.services.InfusionsoftAuthenticationService;
+import org.apereo.cas.infusionsoft.services.UserService;
+import org.apereo.cas.infusionsoft.support.AppHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.security.auth.login.AccountLockedException;
 import javax.security.auth.login.CredentialExpiredException;
 import javax.security.auth.login.FailedLoginException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.*;
 
 /**
  * Infusionsoft implementation of the authentication handler.
@@ -18,12 +29,20 @@ import java.security.GeneralSecurityException;
 @Component
 public class InfusionsoftAuthenticationHandler extends AbstractUsernamePasswordAuthenticationHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(InfusionsoftAuthenticationHandler.class);
+
     public InfusionsoftAuthenticationHandler() {
         super("Infusionsoft Authentication Handler", null, null, null);
     }
 
     @Autowired
+    private AppHelper appHelper;
+
+    @Autowired
     private InfusionsoftAuthenticationService infusionsoftAuthenticationService;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     protected HandlerResult authenticateUsernamePasswordInternal(UsernamePasswordCredential credentials, String originalPassword) throws GeneralSecurityException {
@@ -54,10 +73,53 @@ public class InfusionsoftAuthenticationHandler extends AbstractUsernamePasswordA
                 case PasswordExpired:
                     throw new CredentialExpiredException("login.passwordExpired");
                 case Success:
-                    return this.createHandlerResult(credentials, this.principalFactory.createPrincipal(credentials.getUsername()), null);
+                    return buildHandlerResult(loginResult, credentials);
                 default:
                     throw new IllegalStateException("Unknown value for loginResult: " + loginResult);
             }
         }
+    }
+
+    private HandlerResult buildHandlerResult(LoginResult loginResult, UsernamePasswordCredential credential) {
+        User user = loginResult.getUser();
+
+        Map<String, Object> attributes = new HashMap<>();
+
+        if (user != null) {
+            attributes.put("id", user.getId());
+            attributes.put("displayName", user.getFirstName() + " " + user.getLastName());
+            attributes.put("firstName", user.getFirstName());
+            attributes.put("lastName", user.getLastName());
+            attributes.put("email", user.getUsername());
+
+            // We use a query instead of user.getAccounts() so that we only include enabled accounts
+            List<UserAccount> accounts = userService.findActiveUserAccounts(user);
+            attributes.put("accounts", getAccountsJSON(accounts));
+            attributes.put("authorities", user.getAuthorities());
+        } else {
+            LOGGER.error("User is missing on login result");
+        }
+
+        return this.createHandlerResult(credential, this.principalFactory.createPrincipal(credential.getUsername(), attributes) , null);
+    }
+
+    /**
+     * *************************************************************************************************
+     * * * WARNING * * *
+     * If the format/content of this JSON ever changes in a way that affects parsing on the receiving end,
+     * the TICKETGRANTINGTICKET table needs to be completely cleared, since the old tickets stored there
+     * will still have the old format
+     * **************************************************************************************************
+     */
+    private String getAccountsJSON(List<UserAccount> accounts) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            UserAccountDTO[] userAccounts = UserAccountDTO.convertFromCollection(accounts, appHelper);
+            objectMapper.writeValue(outputStream, userAccounts);
+        } catch (IOException e) {
+            LOGGER.error("Error while serializing accounts to JSON", e);
+        }
+        return outputStream.toString();
     }
 }
