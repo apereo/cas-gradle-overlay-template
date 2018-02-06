d5d9b644c0ad4eb1c5244b85079332178379aa2a
Misagh Moayyed <mmoayyed@unicon.net>
2018-02-06 14:56:39 +0300

    fix mvnw refs

--- build.sh
+++ build.sh
@@ -12,27 +12,27 @@ function copy() {
 function help() {
 	echo "Usage: build.sh [copy|clean|package|run|debug|bootrun|gencert]"
 	echo "	copy: Copy config from ./etc/cas/config to /etc/cas/config"
-	echo "	clean: Clean Maven build directory"
+	echo "	clean: Clean build directory"
 	echo "	package: Clean and build CAS war, also call copy"
-	echo "	run: Build and run cas.war via spring boot (java -jar target/cas.war)"
+	echo "	run: Build and run cas.war via Spring Boot (java -jar target/cas.war)"
 	echo "	runalone: Build and run cas.war on its own (target/cas.war)"
 	echo "	debug: Run CAS.war and listen for Java debugger on port 5000"
-	echo "	bootrun: Run with maven spring boot plugin, doesn't work with multiple dependencies"
+	echo "	bootrun: Run with the Spring Boot plugin, doesn't work with multiple dependencies"
 	echo "	gencert: Create keystore with SSL certificate in location where CAS looks by default"
 	echo "	command: Run the CAS command line shell and pass commands"
 }
 
 function clean() {
-	./mvnw clean "$@"
+	./gradlew clean "$@"
 }
 
 function package() {
-	./mvnw clean package -T 5 "$@"
+	./gradlew clean build "$@"
 	copy
 }
 
 function bootrun() {
-	./mvnw clean package spring-boot:run -T 5 "$@"
+	./gradlew clean build bootRun "$@"
 }
 
 function debug() {
